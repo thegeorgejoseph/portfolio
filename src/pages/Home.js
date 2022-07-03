e0eb02d1fed2cb7e37a58301d819e7ae991ded41
George Joseph <thegeorgejoseph@gmail.com>
2022-07-03 14:38:03 -0700

    Adding underline svg to build

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import underline from "../assets/underline.svg";
 
 const Home = () => {
   return (
@@ -7,6 +8,11 @@ const Home = () => {
       <div className="intro">
         <div className="name">George Joseph</div>
         <div className="name-secondary">Software Engineer</div>
+        <img
+          src={underline}
+          alt="underline"
+          style={{ marginTop: "20px" }}
+        ></img>
       </div>
       <div className="hero">
         <div className="hero-bisect"></div>
